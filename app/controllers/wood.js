c1931f86bd8e8fb2e2dc6b6a4dffc9e14b55482e
kenza17-urc <khammoudi17@gmail.com>
2024-03-20 11:17:00 +0100

    liste des bois

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -1,4 +1,11 @@
-exports.readAll = (req, res) => {
-   
-    res.send('List of woods');
+const { Wood } = require("../models");
+exports.readAll = async (req, res) => {
+  try{
+    const woods = await Wood.findAll();
+    res.status(200).json(woods);
+  }catch{
+    res.status(500).json({
+        message: error.message || "Some error occurred while creating new user.",
+      });
+  }
 }
