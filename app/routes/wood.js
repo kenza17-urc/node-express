c6b71f5dc2328d04c2919b0ebd9a8022f3128d52
kenza17-urc <khammoudi17@gmail.com>
2024-03-20 13:41:20 +0100

    middleware1

--- app/routes/wood.js
+++ app/routes/wood.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
+const auth = require("../middleware/auth.js")
 
 const woodCtrl = require("../controllers/wood.js");
 
-router.get("/", woodCtrl.readAll);
-router.get("/:hardness", woodCtrl.readAll);
+router.get("/", auth,woodCtrl.readAll);
+router.get("/hardness/:hardness",auth, woodCtrl.readAll);
 module.exports = router;
